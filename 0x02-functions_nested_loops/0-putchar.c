6db18053887674061b74f0e7aa5c9b904fdf9bf2
Modasoka <kaytujohn254@gmail.com>
2022-09-14 05:16:45 -0700

    new correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
-/*
- * main - entry point for execution of program
- * 
- * Return: 0
+/**
+ * main - starting point for program
+ *
+ * Description: project description and what it entails
+ *
+ * Return: 0;
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return(0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
