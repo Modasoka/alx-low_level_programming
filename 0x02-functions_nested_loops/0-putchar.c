d1420a3d48eec480210782e8c48278cad7c7c2cf
Modasoka <kaytujohn254@gmail.com>
2022-09-14 14:45:01 +0300

    corrections file 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
-/**
+/*
  * main - entry point for execution of program
  * 
- * Return: always 0 (Success)
- * 
+ * Return: 0
  */
-
 int main(void)
 {
-    putchar("_");
-    putchar("p");
-    putchar("u");
-    putchar("t");
-    putchar("c");
-    putchar("h");
-    putchar("a");
-    putchar("r");
-    putchar("\n");
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
     return(0);
-
 }
