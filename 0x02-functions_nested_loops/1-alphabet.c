e82ddabe3f74e83c2881556134bafe5190042d82
Modasoka <kaytujohn254@gmail.com>
2022-09-14 20:02:06 +0300

    alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,3 +4,12 @@
  * 
  * Return: 0-success, -1 -error
  */
+void print_alphabet(void)
+{
+ char alphabet;
+ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+ {
+    _putchar(alphabet);
+ }
+ _putchar("\n");
+}
