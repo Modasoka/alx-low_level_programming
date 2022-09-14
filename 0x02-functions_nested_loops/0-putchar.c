ded6d2f135947799051e94db64368e67aaa831cf
Modasoka <kaytujohn254@gmail.com>
2022-09-14 14:25:57 +0300

    0putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for execution of program
+ * 
+ * Return: always 0 (Success)
+ * 
+ */
+
+int main(void)
+{
+    putchar("_");
+    putchar("p");
+    putchar("u");
+    putchar("t");
+    putchar("c");
+    putchar("h");
+    putchar("a");
+    putchar("r");
+    putchar("\n");
+
+    return(0);
+
+}
