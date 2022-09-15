217f7f1158cdb972ba9dad16cc30590e722320fb
Modasoka <kaytujohn254@gmail.com>
2022-09-14 18:20:20 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day, starting from 00:00 - 23:59
+ *
+ * Return: prints time, 0 otherwise
+ *
+ */
+void jack_bauer(void)
+
+{
+
+	int minute;
+
+	int hour;
+
+
+
+	for (hour = 0; hour < 24; hour++)
+
+	{
+
+		for (minute = 0; minute < 60; minute++)
+
+		{
+
+			_putchar((hour / 10) + '0');
+
+			_putchar((hour % 10) + '0');
+
+			_putchar(':');
+
+			_putchar((minute / 10) + '0');
+
+			_putchar((minute % 10) + '0');
+
+			_putchar('\n');
+
+		}
+
+	}
+
+}
