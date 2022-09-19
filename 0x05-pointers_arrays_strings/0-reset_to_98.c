6747abf4c2ac0cfe368c72663448354aa0748c71
Modasoka <kaytujohn254@gmail.com>
2022-09-19 06:22:48 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * main - execution of program begins at this point
+ * reset_to_98 - doea a reset of the value n to 98
  * @n: variable set to be manipulated
  * Return: no value (nothing)
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98 /*  */
+	*n = 98; /* alters value of variable that has its address saved */
+}
