d98c76baccbde3cafb7a4dbbe8dcec4dfec6dd20
Modasoka <kaytujohn254@gmail.com>
2022-09-20 18:58:54 +0300

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,44 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _atoi - function that converts a string to an interger
+ * @s: string to be converted to interger
+ * Return: the converted string, otherwise 0 if no numbers
+ *         are present in string
  */
 
 int _atoi(char *s)
-{
-  int a (const char *s);
 
-  return (0);
+{
+  short boolean;
+  int i, minus, result;
+
+
+  i = minus = result = boolean = 0;
+
+  minus = -1;
+
+  while (s[i] != '\0')
+    {
+
+      if (s[i] == '-')
+
+	minus *= -1;
+      if (s[i] >= '0'&& s[i] <= '9')
+	{
+
+	  result *= 10;
+	  result -= (s[i] - '0');
+	  boolean = 1;
+
+	}
+      else if (boolean == 1)
+
+	break;
+
+      i++;
+    }
+  result *= minus;
+
+  return (result);
 }
