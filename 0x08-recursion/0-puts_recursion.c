2c61571426bec4e1a2cf99a60dc78c8d9ed953cd
Modasoka <kaytujohn254@gmail.com>
2022-09-28 09:09:51 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,13 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * _puts_recursion: function that prints string fillowed by new line
+ * _puts_recursion: function that prints string followed by new line
  * @s: string to be printed by the above function
- *
+ * Return: returns no value
  */
 
 void _puts_recursion(char *s)
 {
-	
+
 }
