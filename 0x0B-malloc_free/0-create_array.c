6d9790521c3c0232911a6f7c446679947faef119
Modasoka <kaytujohn254@gmail.com>
2022-10-04 14:56:47 +0300

    corrections

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,16 +2,14 @@
 #include "main.h"
 
 /**
- * 
- * 
+ *
+ *
  */
 
 char *create_array(unsigned int size, char c)
 {
     char *str;
     unsigned int i;
-
-    str = malloc(sizeof(char) * size);
-
     
+    str = malloc(sizeof(char) * size);
 }
