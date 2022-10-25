8aa5ef314010ac9ff8c6771956a7220963a9b8f0
Modasoka <kaytujohn254@gmail.com>
2022-10-24 19:27:33 -0700

    function that returns the number of ellements in a linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,7 +10,6 @@
  * @str: string that has been malloc_checked
  * @len: string length
  * @next: points to the node that comes after
- * 
  * Description: a structure of a singly linked list node
 */
 
@@ -22,10 +21,10 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-size_t liste_len(const list_t *h);
+size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
 
-#endif 
+#endif
