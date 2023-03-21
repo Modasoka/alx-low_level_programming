2c8f39f8e21199d3b112b7af353e83874163153c
WambuaJoe <kaytujohn254@gmail.com>
2023-03-21 08:38:27 +0300

    test file

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - check code
+ *
+ * Return: always EXIT_SUCCESS.
+*/
+
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+}
