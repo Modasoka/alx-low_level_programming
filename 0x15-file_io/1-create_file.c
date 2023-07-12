d95880de5b21b070747386f66c8f5bfbb96eb352
WambuaJoe <kaytujohn254@gmail.com>
2023-07-12 20:37:51 +0300

    a function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: file name
+ * @text_content: the content to be placed in the file
+ *
+ * Return: returns 1 on success otherwise -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+	int i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	fp = fopen(filename, "w");
+
+	if (fp == NULL)
+		return (-1);
+
+	chmod(filename, 0600);
+
+	while (text_content[i] != '\0')
+	{
+		putc(text_content[i], fp);
+		i++;
+	}
+	fclose(fp);
+	return (1);
+}
+
