10464ee3d99eec7cc9ee149b6678f60001dd9eaf
WambuaJoe <kaytujohn254@gmail.com>
2023-09-14 07:05:56 +0300

    code fixes & edits
    - modify header file
    - add main.c file to cross check
    - modify target file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all elements of a doubly linked list
- * @h: pointer to head of list
- *
+ * print_dlistint - function prints all elements of doubly linked list
+ * @h: the ptr to the head of list
  * Return: number of nodes in list
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+	size_t counter = 0;
 
 	if (h == NULL)
 		return (0);
@@ -17,9 +16,9 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
+		counter++;
 		h = h->next;
 	}
 
-	return (count);
+	return (counter);
 }
