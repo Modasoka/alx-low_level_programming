0813206779480c8d88602deffda7f75e029e6c7d
WambuaJoe <kaytujohn254@gmail.com>
2023-10-06 15:05:00 +0300

    code fix - header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -9,10 +9,11 @@
  * struct hash_node_s - Node of a hash table
  *
  * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
+ * key is unique in the HashTable
+ * @value: value corresponding to a key
+ * @next: A ptr to next node of the List
  */
+
 typedef struct hash_node_s
 {
 	char *key;
